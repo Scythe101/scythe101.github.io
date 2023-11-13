35106ebc4369471dcedb0c46ed95c37fe58f5748
Scythe101 <127077495+Scythe101@users.noreply.github.com>
2023-11-13 22:16:17 +0000

    changed menu button interactions a bit

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 var isOpen=false;
 var isOver=false;
+function controlNav(){
+  if (isOpen){
+    closeNav();
+  }
+  else{
+    openNav();
+  }
+}
 function openNav() {
+  
   document.getElementById("mySidenav").style.width = "250px";
   isOpen=true;
   document.getElementById("mainContainer").style.opacity = "0.4";
   document.getElementById("mainContainer").style.filter= "blur(2px)";
+  document.getElementById("menuButton").style.color= "#ced4de";
+  
 }
   
 function closeNav() {
@@ -12,12 +23,16 @@ function closeNav() {
   document.getElementById("mainContainer").style.filter= "blur(0)";
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("menuButton").style.position = "fixed";
+  document.getElementById("menuButton").style.color = "#3B4252";
   isOpen=false;
   
 }
 
 function teaseNav(){
-  document.getElementById("mySidenav").style.width = "7px";
+  if (isOpen!==true){
+    document.getElementById("mySidenav").style.width = "7px";
+  }
+  
 }
 
 function closeTeasedNav(){
